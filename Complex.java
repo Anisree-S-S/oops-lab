874a0d4bfd8b21858964d92956510f8e20d17317
Anisree-S-S <96193534+Anisree-S-S@users.noreply.github.com>
2022-03-23 11:50:27 +0530

    to add two complex numbers

--- Complex.java
+++ Complex.java
@@ -7,14 +7,14 @@ class Complex
 		Scanner s=new Scanner(System.in);
 		System.out.println("enter the real part of first complex no:");
 		a=s.nextInt();
-		System.out.println("enter the img part of first complex no:");
+		System.out.println("enter the imaginary part of first complex no:");
 		b=s.nextInt();
 		System.out.println("enter the real part of second complex no:");
 		c=s.nextInt();
-		System.out.println("enter the img part of first complex no:");
+		System.out.println("enter the imaginary part of second complex no:");
 		d=s.nextInt();
 		e=a+c;
 		f=b+d;
-		System.out.println("Sum of given complex no: is  : "+e+"+"+f+"i");
+		System.out.println("Sum of given complex number is  : "+e+"+"+f+"i");
 	}
 }
