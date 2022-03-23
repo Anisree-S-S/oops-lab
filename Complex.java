90aed98cb7b71d2b4c9cda4f7c5d6acc51b067d5
Anisree-S-S <96193534+Anisree-S-S@users.noreply.github.com>
2022-03-23 11:45:50 +0530

    to add two complex numbers

---
+++ Complex.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Complex
+{
+	public static void main(String[] args)
+	{
+		int a,b,c,d,e,f;
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter the real part of first complex no:");
+		a=s.nextInt();
+		System.out.println("enter the img part of first complex no:");
+		b=s.nextInt();
+		System.out.println("enter the real part of second complex no:");
+		c=s.nextInt();
+		System.out.println("enter the img part of first complex no:");
+		d=s.nextInt();
+		e=a+c;
+		f=b+d;
+		System.out.println("Sum of given complex no: is  : "+e+"+"+f+"i");
+	}
+}
