cfd2f99e4c03cb39075e60f6b59a0cca8a4611a0
Anisree-S-S <96193534+Anisree-S-S@users.noreply.github.com>
2022-04-25 14:50:21 +0530

    String manipulation

--- CO2/StringManip.java
+++ CO2/StringManip.java
@@ -7,10 +7,10 @@ class StringManip
 		Scanner sc=new Scanner(System.in);
 		String str1=sc.nextLine();
 		System.out.println("length of string\t:  "+str1.length());
-		System.out.println("string contains col sequence\t:  "+str1.contains("Col"));
+		System.out.println("string contains 'col' sequence\t:  "+str1.contains("Col"));
 		System.out.println("character at first position\t :  "+str1.charAt(1));
-		System.out.println("String ends with e\t:  "+str1.endsWith("e"));
-		System.out.println("replace col with kol\t:  " +str1.replaceAll("Col","kol"));
+		System.out.println("String ends with 'e'\t:  "+str1.endsWith("e"));
+		System.out.println("replace 'col' with 'kol'\t:  " +str1.replaceAll("Col","kol"));
 		System.out.println("LOWERCASE\t:  "+str1.toLowerCase());
 		System.out.println("UPPERCASE\t:  "+str1.toUpperCase());
 		sc.close();
