bc800fae39831420ad06353639e113da1689c819
Anisree-S-S <96193534+Anisree-S-S@users.noreply.github.com>
2022-06-06 15:11:34 +0530

    inheritance

---
+++ co3/multilevelInheritance.java
@@ -0,0 +1,63 @@
+import java.util.*;
+class Employee
+{
+	int empid;
+	String name;
+	double salary;
+	String address;
+	Employee()
+	{
+		Scanner sc2=new Scanner(System.in);
+		System.out.println("enter employee   id:");
+		empid= sc2.nextInt();
+		System.out.println("enter employee  name:");
+		name= sc2.next();
+		System.out.println("enter employee  salary:");
+		salary= sc2.nextDouble();
+		System.out.println("enter employee  address:");
+		address= sc2.next();
+	}
+}
+class Teacher extends Employee
+{
+	String department;
+	String subject;
+	Teacher()
+	{
+		Scanner sc3=new Scanner(System.in);
+		System.out.println("enter  department:");
+		department= sc3.next();
+		System.out.println("enter  subject:");
+		subject= sc3.next();
+	}
+	void display()
+	{
+		System.out.println("................................");
+		System.out.println("ID\t:" +empid);
+		System.out.println("NAME\t:" +name);
+		System.out.println("SALARY\t:" +salary);
+		System.out.println("ADDRESS\t:" +address);
+		System.out.println("DEPARTMENT\t:" +department);
+		System.out.println("SUBJECT\t:" +subject);
+		System.out.println("...............................");
+	}
+}
+class multilevelInheritance
+{
+	public static void main(String args[])
+	{
+		int n;
+		Scanner sc1=new Scanner(System.in);
+		System.out.println("enter number of employees:");
+		n=sc1.nextInt();
+		Teacher t[]= new Teacher[n];
+		for(int i=0;i<n;i++)
+			t[i]=new Teacher();
+		System.out.println("...............................");
+		System.out.println("...............................");
+		System.out.println("THE DETAILS ARE...:");
+		System.out.println("...............................");
+		for(int i=0;i<n;i++)
+			t[i].display();
+	}  
+}
