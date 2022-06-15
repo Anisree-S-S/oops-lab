c2a5c541898306277818c83ad57cc5e253a7915d
Anisree-S-S <96193534+Anisree-S-S@users.noreply.github.com>
2022-06-15 12:14:17 +0530

    Add files via upload

---
+++ CO4/Package/Graphics/Rectangle1.java
@@ -0,0 +1,18 @@
+package Graphics;
+import java.io.*;
+import java.util.*;
+public class Rectangle1
+{
+	int l,b;
+	Scanner sc=new Scanner(System.in);
+	public void getdata()
+	{
+		System.out.println("Enter the length and breadth ");
+		l=sc.nextInt();
+		b=sc.nextInt();
+	}
+	public void area()
+	{
+		System.out.println("Area of rectangle is\t:"+(l*b));
+	}
+}
